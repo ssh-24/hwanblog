30e889ce639258c8d40e0d545d86b4829d0686c3
Hwan <65848753+ssh-24@users.noreply.github.com>
2022-10-23 17:27:46 +0900

    221023 17:27

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const nav_title = "Hwan Blog";
+  let [title1, setTitle1] = useState('오늘 저녁 리뷰');
+  let [content1, setContent1] = useState('고기를 먹었다. 맛있었다. 고기는 최고야');
+  let [title2, setTitle2] = useState('내일 점심 뭐먹지');
+  let [content2, setContent2] = useState('저녁먹고 다음날 점심을 고민하는 나, 돼지인걸까?');
+  let [title3, setTitle3] = useState('그냥 뻘글');
+  let [content3, setContent3] = useState('블로그는 원래 생각없이 쓰는거야');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <h4>{nav_title}</h4>
+      </div>
+      <div className='post'>
+        <h4>{title1}</h4>
+        <p>{content1}</p>
+      </div>
+      <div className='post'>
+        <h4>{title2}</h4>
+        <p>{content2}</p>
+      </div>
+      <div className='post'>
+        <h4>{title3}</h4>
+        <p>{content3}</p>
+      </div>
     </div>
   );
 }
