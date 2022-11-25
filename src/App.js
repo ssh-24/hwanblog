cc126dd38ea7090deaf045dd1c32a42a4af744c9
ssh-24 <sonsh5364@gmail.com>
2022-11-25 14:50:49 +0900

    221125 14:50

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
+/*eslint-disable*/
+
 import './App.css';
 import { useState } from 'react';
 
 function App() {
   const nav_title = "Hwan Blog";
-  let [title1, setTitle1] = useState('오늘 저녁 리뷰');
-  let [content1, setContent1] = useState('고기를 먹었다. 맛있었다. 고기는 최고야');
-  let [title2, setTitle2] = useState('내일 점심 뭐먹지');
-  let [content2, setContent2] = useState('저녁먹고 다음날 점심을 고민하는 나, 돼지인걸까?');
-  let [title3, setTitle3] = useState('그냥 뻘글');
-  let [content3, setContent3] = useState('블로그는 원래 생각없이 쓰는거야');
+  let [title, setTitle] = useState(['오늘 저녁 리뷰','내일 점심 뭐먹지','그냥 뻘글']);
+  let [content, setContent] = useState(['고기를 먹었다. 맛있었다. 고기는 최고야'
+  ,'저녁먹고 다음날 점심을 고민하는 나, 돼지인걸까?'
+  ,'블로그는 원래 생각없이 쓰는거야']);
 
   return (
     <div className="App">
@@ -17,16 +16,16 @@ function App() {
         <h4>{nav_title}</h4>
       </div>
       <div className='post'>
-        <h4>{title1}</h4>
-        <p>{content1}</p>
+        <h4>{title[0]}</h4>
+        <p>{content[0]}</p>
       </div>
       <div className='post'>
-        <h4>{title2}</h4>
-        <p>{content2}</p>
+        <h4>{title[1]}</h4>
+        <p>{content[1]}</p>
       </div>
       <div className='post'>
-        <h4>{title3}</h4>
-        <p>{content3}</p>
+        <h4>{title[2]}</h4>
+        <p>{content[2]}</p>
       </div>
     </div>
   );
